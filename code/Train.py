4986a6d4433feec57b09fb77cb0581fb68f1e967
Tristan Engst <tristanengst@gmail.com>
2022-12-03 05:44:50 -0800

    Fixes

--- code/Train.py
+++ code/Train.py
@@ -75,7 +75,7 @@ if __name__ == "__main__":
     base_optimizer = torch.optim.SGD
 
     if args.opt == "sgd":
-        optimizer = SGD(model.parameters(), lr=args.lr, momentum=args.momentum, weight_decay=args.weight_decay)
+        optimizer = torch.optim.SGD(model.parameters(), lr=args.lr, momentum=args.momentum, weight_decay=args.weight_decay)
     elif args.opt == "sam":
         optimizer = SAM(model.parameters(), base_optimizer, rho=args.rho, adaptive=args.adaptive, lr=args.lr, momentum=args.momentum, weight_decay=args.weight_decay)
     elif args.opt == "msam":
