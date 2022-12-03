e0a9ee8986aefcc1a7e2b5dd0e84255dcb36773a
Tristan Engst <tristanengst@gmail.com>
2022-12-03 05:41:14 -0800

    Update for hyperparameter tuning (may need fixes)

--- code/wandb/run-20221203_053540-2ek8evbv/files/code/code/Train.py
+++ code/wandb/run-20221203_053540-2ek8evbv/files/code/code/Train.py
@@ -1,5 +1,7 @@
 import argparse
+import numpy as np
 import torch
+from tqdm import tqdm
 
 from model.wide_res_net import WideResNet
 from model.smooth_cross_entropy import smooth_crossentropy
@@ -16,10 +18,10 @@ import wandb
 
 def get_name(args):
     suffix = f"-{arg.suffix}" if not args.suffix is None else ""
-    return f"{args.task}-adapt{int(args.adaptive)}-gamma{args.gamma}-lr{args.lr}-rho{args.rho}-{args.uid}{suffix}"
+    return f"{args.opt}-adapt{int(args.adaptive)}-gamma{args.gamma}-lr{args.lr}-rho{args.rho}-{args.uid}{suffix}"
 
 if __name__ == "__main__":
-    P = argparse.ArgumentP()
+    P = argparse.ArgumentParser()
     P.add_argument("--wandb", default="disabled", choices=["disabled", "online"],
         help="WandB usage")
     P.add_argument("--opt", choices=["sgd", "sam", "msam"], required=True,
@@ -38,11 +40,11 @@ if __name__ == "__main__":
         help="Total number of epochs.")
     P.add_argument("--label_smoothing", default=0.1, type=float,
         help="Use 0.0 for no label smoothing.")
-    P.add_argument("--learning_rate", default=0.1, type=float,
+    P.add_argument("--lr", default=0.1, type=float,
         help="Base learning rate at the start of the training.")
     P.add_argument("--momentum", default=0.9, type=float,
         help="SGD Momentum.")
-    P.add_argument("--threads", default=2, type=int,
+    P.add_argument("--threads", default=20, type=int,
         help="Number of CPU threads for dataloaders.")
     P.add_argument("--rho", default=.05, type=float,
         help="Rho parameter for SAM.")
@@ -50,8 +52,11 @@ if __name__ == "__main__":
         help="L2 weight decay.")
     P.add_argument("--width_factor", default=8, type=int,
         help="How many times wider compared to normal ResNet.")
+    P.add_argument("--suffix", default=None, type=str,
+        help="Optional suffix")
     args = P.parse_args()
     args.uid = wandb.util.generate_id()
+    tqdm.write(str(args))
 
     run = wandb.init(project="MomentumSAM",
         anonymous="allow",
@@ -61,7 +66,7 @@ if __name__ == "__main__":
         mode=args.wandb)
 
     initialize(args, seed=42)
-    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
     dataset = Cifar(args.batch_size, args.threads)
     log = Log(log_each=10)
@@ -70,19 +75,25 @@ if __name__ == "__main__":
     base_optimizer = torch.optim.SGD
 
     if args.opt == "sgd":
-        optimizer = SGD(model.parameters(), lr=args.learning_rate, momentum=args.momentum, weight_decay=args.weight_decay)
+        optimizer = SGD(model.parameters(), lr=args.lr, momentum=args.momentum, weight_decay=args.weight_decay)
     elif args.opt == "sam":
-        optimizer = SAM(model.parameters(), base_optimizer, rho=args.rho, adaptive=args.adaptive, lr=args.learning_rate, momentum=args.momentum, weight_decay=args.weight_decay)
+        optimizer = SAM(model.parameters(), base_optimizer, rho=args.rho, adaptive=args.adaptive, lr=args.lr, momentum=args.momentum, weight_decay=args.weight_decay)
     elif args.opt == "msam":
-        optimizer = MSAM(model.parameters(), base_optimizer, gamma=args.gamma, rho=args.rho, adaptive=args.adaptive, lr=args.learning_rate, momentum=args.momentum, weight_decay=args.weight_decay)
-    scheduler = StepLR(optimizer, args.learning_rate, args.epochs)
+        optimizer = MSAM(model.parameters(), base_optimizer, gamma=args.gamma, rho=args.rho, adaptive=args.adaptive, lr=args.lr, momentum=args.momentum, weight_decay=args.weight_decay)
+    scheduler = StepLR(optimizer, args.lr, args.epochs)
 
     for epoch in range(args.epochs):
+
+        # Things we log for hyperparameter tuning
+        losses_tr = []
+        losses_te = []
+        accs_te = []
+
         model.train()
         log.train(len_dataset=len(dataset.train))
 
         for batch in dataset.train:
-            inputs, targets = (b.to(device) for b in batch)
+            inputs, targets = (b.to(device, non_blocking=True) for b in batch)
 
             # first forward-backward step
             enable_running_stats(model)
@@ -96,6 +107,8 @@ if __name__ == "__main__":
             smooth_crossentropy(model(inputs), targets, smoothing=args.label_smoothing).mean().backward()
             optimizer.second_step(zero_grad=True)
 
+            losses_tr.append(loss.detach())
+
             with torch.no_grad():
                 correct = torch.argmax(predictions.data, 1) == targets
                 log(model, loss.cpu(), correct.cpu(), scheduler.lr())
@@ -104,13 +117,28 @@ if __name__ == "__main__":
         model.eval()
         log.eval(len_dataset=len(dataset.test))
 
+
         with torch.no_grad():
             for batch in dataset.test:
-                inputs, targets = (b.to(device) for b in batch)
+                inputs, targets = (b.to(device, non_blocking=True) for b in batch)
 
                 predictions = model(inputs)
                 loss = smooth_crossentropy(predictions, targets)
                 correct = torch.argmax(predictions, 1) == targets
                 log(model, loss.cpu(), correct.cpu())
+
+                losses_te.append(loss)
+                accs_te.append(correct)
+
+
+        acc_te = torch.cat(accs_te).float().mean()
+        loss_te = torch.cat(losses_te).cpu().mean()
+        loss_tr = torch.cat(losses_tr).mean()
+
+        wandb.log({"epoch": epoch,
+            "loss/te": loss_te,
+            "acc/te": acc_te,
+            "loss_tr": loss_tr})
+
 
     log.flush()
