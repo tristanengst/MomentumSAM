f1a11ca650098a7effaf76e52f467042e90f951a
Tristan Engst <tristanengst@gmail.com>
2022-12-09 19:44:14 -0800

    Updating SLURM script

--- slurm/SGD.sh
+++ slurm/SGD.sh
@@ -2,7 +2,7 @@
 
 # The SBATCH directives must appear before any executable line in this script.
 
-#SBATCH --array=0-10%5
+#SBATCH --array=0-9%5
 #SBATCH --time=0-11:0:00               # Time: D-H:M:S
 #SBATCH --account=def-keli              # Account: def-keli/rrg-keli
 #SBATCH --mem=120G                       # Memory in total
@@ -37,7 +37,7 @@ source ~/virtual_envs/py310MSAM/bin/activate
 export PYTHONUNBUFFERED=1
 export MKL_SERVICE_FORCE_INTEL=1
 
-cd ~/projects/def-keli/tme3/MomentumSAM/code
+cd ~/projects/def-keli/tme3/MomentumSAM
 wandb agent --count 1 tristanengst/NNSAM/p8ser26u
 
 # Print completion time.
