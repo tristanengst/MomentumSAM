6eb1bd3f7d639a6e4fc9c545d406e2768e0a1e56
Tristan Engst <tristanengst@gmail.com>
2022-12-09 20:16:01 -0800

    Update

--- slurm/SGD.sh
+++ slurm/SGD.sh
@@ -38,7 +38,7 @@ export PYTHONUNBUFFERED=1
 export MKL_SERVICE_FORCE_INTEL=1
 
 cd ~/projects/def-keli/tme3/NNSAM
-wandb agent --count 1 tristanengst/NNSAM/p8ser26u
+wandb agent  --count 1 tristanengst/NNSAM/y4mzp0ou
 
 # Print completion time.
 date
