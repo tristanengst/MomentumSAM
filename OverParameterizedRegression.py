6ce17d2b10ac26bfb923fc79120f57067f85e6b9
Tristan Engst <tristanengst@gmail.com>
2022-12-15 05:11:12 -0800

    Faster on CPU!

--- OverParameterizedRegression.py
+++ OverParameterizedRegression.py
@@ -120,7 +120,8 @@ def get_args():
 if __name__ == "__main__":
     args = get_args()
     initialize(args, seed=args.seed)
-    device = torch.device(f"cuda:{args.device_id}" if torch.cuda.is_available() else "cpu")
+    # device = torch.device(f"cuda:{args.device_id}" if torch.cuda.is_available() else "cpu")
+    device = torch.device("cpu")
 
     run = wandb.init(project="NNSAM",
         anonymous="allow",
